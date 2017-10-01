bc7340a82259a4e7d8b09c4dc727b88df868286c
Fabian <copy@copy.sh>
2017-10-01 18:55:02 +0200

    Update pulseaudio volume setting script

--- .local/bin/setvolume.sh
+++ .local/bin/setvolume.sh
@@ -1,67 +1,18 @@
 #!/bin/bash
 
 
-#if [[ $1 == "increase" ]]
-#then
-#    #amixer set Master 7%+
-#    amixer -c0 set Master 7%+
-#    amixer -c1 set Master 7%+
-#    amixer -c2 set Master 7%+
-#elif [[ $1 == "decrease" ]]
-#then
-#    #amixer set Master 7%-
-#    amixer -c0 set Master 7%-
-#    amixer -c1 set Master 7%-
-#    amixer -c2 set Master 7%-
-#elif [[ $1 == "mute" ]]
-#then
-#    #amixer set Master toggle
-#    amixer -c0 set Master toggle
-#    amixer -c1 set Master toggle
-#    amixer -c2 set Master toggle
-#fi
-#
-#
-#
-#exit
+SINKS=$(pactl list sinks|grep -oh 'Name: .*'|cut -d ' ' -f2)
 
+CURVOL=$(pactl list sinks |grep "Volume:"|grep -v "Base"|grep -oh '\([0-9]*\)%'|tail -n1|tr % ' ')
 
-# pulseaudio method starts below
-
-#mkdir -p /tmp/.pulse/
-
-#### Create /tmp/.pulse/mute if not exists
-#ls /tmp/.pulse/mute &> /dev/null
-#if [[ $? != 0 ]]
-#then
-#    echo "false" > /tmp/.pulse/mute
-#fi
-
-####Create /tmp/.pulse/volume if not exists
-#ls /tmp/.pulse/volume &> /dev/null
-#if [[ $? != 0 ]]
-#then
-    #echo "65536" > /tmp/.pulse/volume
-#fi
-
-
-# Find with: pactl list sinks|grep 'Name:'
-#SINK=alsa_output.pci-0000_05_00.0.analog-surround-51
-#SINK=alsa_output.pci-0000_00_1b.0.analog-surround-51
-#SINK=alsa_output.pci-0000_00_1b.0.analog-surround-51
-SINK=$(pactl list sinks|grep -oh 'Name: .*'|cut -d ' ' -f2|head -n1)
-
-#CURVOL=`cat /tmp/.pulse/volume`     #Reads in the current volume
-CURVOL=$(pactl list sinks |grep "Volume:"|grep -v "Base"|grep -oh '\([0-9]*\)%'|head -n1|tr % ' ')
 echo Current volume: $CURVOL
 
-#MUTE=`cat /tmp/.pulse/mute`          #Reads mute state
-MUTE=$(pactl list sinks|grep -oh 'Mute: \w*'|cut -b 7-10)
+MUTE=$(pactl list sinks|grep -oh 'Mute: \w*'|cut -b 7-10|tail -n1)
 echo Muted: $MUTE
 
 if [[ $1 == "increase" ]]
 then
-    CURVOL=$(($CURVOL + 5))
+    CURVOL=$(($CURVOL - $CURVOL % 5 + 5))
     if [[ $CURVOL -ge 150 ]]
     then
         CURVOL=150
@@ -69,7 +20,7 @@ then
     notify-send $CURVOL
 elif [[ $1 == "decrease" ]]
 then
-    CURVOL=$(($CURVOL - 5))
+    CURVOL=$(($CURVOL - $CURVOL % 5 - 5))
     if [[ $CURVOL -le 0 ]]
     then
         CURVOL=0
@@ -79,18 +30,23 @@ elif [[ $1 == "mute" ]]
 then
     if [[ $MUTE == "no" ]]
     then
-        pactl set-sink-mute $SINK 1
+        for SINK in $SINKS
+        do
+            pactl set-sink-mute $SINK 1
+        done
         notify-send "Muted"
     exit
     else
-        pactl set-sink-mute $SINK 0
+        for SINK in $SINKS
+        do
+            pactl set-sink-mute $SINK 0
+        done
         notify-send "Not muted"
     exit
     fi
 fi
 
-pactl set-sink-volume $SINK $CURVOL%
-
-#echo $CURVOL
-
-#echo $CURVOL > /tmp/.pulse/volume # Write the new volume to disk to be read the next time the script is run.
+for SINK in $SINKS
+do
+    pactl set-sink-volume $SINK $CURVOL%
+done
